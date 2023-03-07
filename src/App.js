9aa7126025653b3d46da6eb00aa9a90bbcc48b5d
varunpats <varunpatil2502@gmail.com>
2023-03-07 23:44:26 +0530

    Added validation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import AddUser from './components/Users/AddUser'
+import React, { useState } from 'react';
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const onUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      console.log(usersList);
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() }
+      ];
+    })
+  }
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={onUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -17,15 +17,23 @@ const AddUser = props => {
 
     const onSubmitHandler = (event) => {
         event.preventDefault();
+        if(username.trim().length === 0 || age.trim().length === 0)
+            return;
+        if(+age < 0)
+            return;
+        console.log(username, age);
+        props.onAddUser(username, age);
+        setUsername('');
+        setAge('');
     }
 
     return (
         <Card className={classes.input}>
             <form onSubmit={onSubmitHandler}>
                 <label htmlFor='username'>Username</label>
-                <input id="username" type="text" onChange={usernameHandler} />
+                <input id="username" value={username} type="text" onChange={usernameHandler} />
                 <label htmlFor='age'>Age (Years)</label>
-                <input id="age" type="number" onChange={ageHandler} />
+                <input id="age" value={age} type="number" onChange={ageHandler} />
                 <Button type='submit'>Add User</Button>
             </form>
         </Card>
